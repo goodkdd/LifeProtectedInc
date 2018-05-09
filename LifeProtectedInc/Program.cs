ee536b820f18565d520330b274f9b97e0fb220a9
Eudghy Dey Delcy <eudghy@hotmail.com>
2018-05-09 14:18:19 -0300

    table container

--- LifeProtectedInc/Program.cs
+++ LifeProtectedInc/Program.cs
@@ -25,7 +25,7 @@ namespace LifeProtectedInc
                 {
                     var context = services.GetRequiredService<LifeContext>();
                     //Creating a more complex data model - comment out line below
-                    DbInitializer.Initialize(context);
+                    //DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
